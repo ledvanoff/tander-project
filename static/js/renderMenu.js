dfa0ad47600b26b9292e5a86d7240f0e8678c360
Oleg Ledvanov <oleg.ledvanov@yandex.ru>
2019-12-30 02:27:15 +0300

    Menu stylization

--- static/js/renderMenu.js
+++ static/js/renderMenu.js
@@ -10,12 +10,12 @@ const urls = {
 const makeMenuItems = (urls)=>{
     let lis = '';
     for (const [key, value] of Object.entries(urls)) {
-        lis += `<li class="pure-menu-item"><a href="${value}" class="pure-menu-link">${key}</a></li>`
+        lis += `<li class="pure-menu-item"><a href="${value}" class="menu-link pure-menu-link">${key}</a></li>`
     }
     return lis
 }
 const html = `<div class="pure-menu pure-menu-horizontal">
-<a href="${brandLink}" class="pure-menu-heading pure-menu-link">${brandName}</a>
+<strong><a href="${brandLink}" class="menu-link pure-menu-heading pure-menu-link">${brandName}</a></strong>
 <ul class="pure-menu-list">
     ${makeMenuItems(urls)}
 </ul>
